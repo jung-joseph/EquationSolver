07af1c31537b10cd4e359a1914e2916f70d09696
Joseph Jung <joseph.jung@icloud.com>
2020-04-14 17:05:30 -0600

    Initial Commit

---
+++ EquationSolver/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EquationSolver
+//
+//  Created by Joseph Jung on 4/14/20.
+//  Copyright © 2020 Joseph Jung. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
