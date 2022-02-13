5c5cab47cd716a306b886785454bea1cb1892bd8
Joseph Jung <joseph.jung@icloud.com>
2022-02-12 21:52:15 -0700

    V 0.1.0 (4)

--- EquationSolver/Views/AboutView.swift
+++ EquationSolver/Views/AboutView.swift
@@ -67,7 +67,7 @@ struct AboutView: View {
                             .font(.title)
                             .padding(EdgeInsets(top: 25, leading: 25, bottom: 0, trailing: 0))
                         Text("Welcome to AX=B! If you downloaded this app, I know that you are not a typical iOS user! Perhaps you have also noticed as I have, that there appears to be a dearth of BASIC \"Engineering or Science\" tools for students or professionals on the App Store. I have also observed that most college engineering programs (with the notable exception of computer science programs such as Stanford's), do not teach their students how to create programs (or tools) for mobile devices. Therefore, in the spirit of providing useful tools and programming examples of iOS tools for students, I created this simultaneous linear equation solver app. This app is written using SwiftUI (not UIKit). Once I am assured that the major bugs have been found and the app is reasonably easy to use, I intend to make the xCode project available in GitHub.  I hope that you find this app useful. ")
-                            .font( Font.system(size: 15))
+                            .font( Font.system(size: 14))
                             .foregroundColor(Color.black)
                         
                         
@@ -78,8 +78,8 @@ struct AboutView: View {
                             .foregroundColor(Color.black)
                             .font(.title)
                             .padding(EdgeInsets(top: 25, leading: 25, bottom: 0, trailing: 0))
-                        Text("The actual app usage should be intuitive, but some guidance may be useful. The user may either initiate a completely new problem or read-in a problem that he/she had previously entered and saved. Saved problems and saved solutions are stored in Files App on iOS devices in JSON format. Once the problem has been entered (the coefficients of the A matrix and B vector are defined), any one of three solution algorithms may be used. All three algorithms are variants of Gauss elimination with back substituion (1). I included three algorithms to allow the user to explore how these variants perform for different types of problems. The algorithms are, Gauss Elimination with no row interchanges; Gauss Elimination with Maximal Column Pivoting; and Gauss Elimination with Scaled Column Pivoting. To assist the user in evaluating the performance of the different algorithms and the quality of the solution, an Error vector (the difference of each equation's B value and the computed result) is given. The users should always exam the values in the Error vector! In general, the Scaled Column Pivoting algorithm is considered be the most robust in working with poorly conditioned equation systems (1). The solution file may also be exported for subsequent use.")
-                            .font(Font.system(size: 15))
+                        Text("The actual app usage should be intuitive, but some guidance may be useful. The user may either initiate a completely new problem or read-in a problem that he/she had previously entered and saved. Saved problems and saved solutions are stored in Files App on iOS devices in JSON format. Once the problem has been entered (the coefficients of the A matrix and B vector are defined), any one of three solution algorithms may be used. All three algorithms are variants of Gauss elimination with back substituion (1). I included three algorithms to allow the user to explore how these variants perform for different types of problems. The algorithms are: Gauss Elimination with no row interchanges, Gauss Elimination with Maximal Column Pivoting, and Gauss Elimination with Scaled Column Pivoting. To assist the user in evaluating the performance of the different algorithms and the quality of the solution, an Error vector (the difference of each equation's B value and the computed result) is given. The users should always exam the values in the Error vector! In general, the Scaled Column Pivoting algorithm is considered be the most robust in working with poorly conditioned equation systems (1). The solution file may also be exported for subsequent use.")
+                            .font(Font.system(size: 14))
                             .foregroundColor(Color.black)
                         
                         
@@ -91,7 +91,7 @@ struct AboutView: View {
                             .font(.title)
                             .padding(EdgeInsets(top: 25, leading: 25, bottom: 0, trailing: 0))
                         Text("Of course, there cannot be a guarentee that any given numerical algorithm will always \"work.\"  Moreover, even though the implimentations have been extensively tested, I do not assume any responsibility for the results or the usage of the results of this software. The user is reminded to always exam the \"Error\" vector, as it provides a quantitative check of the accuracy of the numerical solution.")
-                            .font( Font.system(size: 15))
+                            .font( Font.system(size: 14))
                             .foregroundColor(Color.black)
                     }
                     
@@ -100,12 +100,20 @@ struct AboutView: View {
                             .foregroundColor(Color.black)
                             .font(.title)
                             .padding(EdgeInsets(top: 25, leading: 25, bottom: 0, trailing: 0))
-                        Text("1. Burden, Richard and Faires, J. Douglas; Numerical Analysis (third edition); Prindle, Weber & Schmit, Boston, 1985. ")
-                            .font( Font.system(size: 15))
+                        HStack{
+                        Text("1. Burden, Richard and Faires, J. Douglas; Numerical Analysis (third edition); Prindle, Weber & Schmit, Boston, 1985.")
+                            .font( Font.system(size: 14))
                             .foregroundColor(Color.black)
-                        Text("2. 3x3 Round-Off Verification Problem: https://www.rajgunesh.com/resources/downloads/numerical/gaussianelimination.pdf")
-                            .font( Font.system(size: 15))
+                            
+                            Spacer()
+                        }
+                        HStack{
+                        Text("2. 3x3 Round-Off Verification Problem; https://www.rajgunesh.com/resources/downloads/numerical/gaussianelimination.pdf")
+                            .font( Font.system(size: 14))
                             .foregroundColor(Color.black)
+                            
+                            Spacer()
+                        }
                         
                     }
                     
